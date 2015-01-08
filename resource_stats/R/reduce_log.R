5bc24b3b95931d1cdfb080418e491b3588e65eee
Jack Viljoen <jack.viljoen@cpgr.org.za>
2015-01-08 15:06:08 +0200

    move data reduction script into R package

---
+++ resource_stats/R/reduce_log.R
@@ -0,0 +1,39 @@
+#' Downsample several columns by process
+#'
+#' Downsample a data frame with >2 columns with the LTTB algorithm,
+#' using the Split-Apply-Combine pattern to downsample the data for
+#' each Process in `data` separately.
+#'
+#' @param data The data frame to downsample. The first column must be
+#'   the Time column and there must be a column named "Process".
+#' @param nbins The (maximum) number of rows of data to return for
+#'   each process.
+#'
+#' @return A data frame of with the same columns as `data` but at
+#'   most `nbins` rows for each unique process in `data$Process`.
+#'
+#' @export
+downsample_multiple_by_process = function(data, nbins) {
+
+  downsample_multiple = function(dat, n) {
+
+    downsample = function(vec, n) {
+      matr = cbind(dat[, 1], vec)
+      LTTB::LTTB(matr, n-2)[, 2]
+    }
+
+    plyr::colwise(downsample)(dat[, -ncol(dat)], n)
+  }
+
+  outdf = as.data.frame(matrix(NA, ncol=ncol(data), nrow=0))
+  names(outdf) = names(data)
+
+  for (p in unique(data$Process)) {
+    data_subset = subset(data, Process==p)
+    downsampled = downsample_multiple(data_subset, nbins)
+    downsampled$Process = p
+    outdf = rbind(outdf, downsampled)
+  }
+
+  return(outdf)
+}
